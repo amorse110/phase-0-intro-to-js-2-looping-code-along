f3abe9961ecca9c35f77f186f2920d4bcbbbc746
Austin Morse <amorse110@gmail.com>
2023-06-15 10:13:42 -0700

    lab complete

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ const gifts = ["teddy bear", "drone", "doll"];
 
 function wrapGifts(gifts) {
     for (let i = 0; i < gifts.length; i++) {
-        console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    }
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
 
-    return gifts;
+return gifts;
 }
 
-let messages = []
+const messages = [];
 
 function writeCards(names, event) {
     for (let i = 0; i < names.length; i++) {
-        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
     }
-    return messages
+
+    return messages;
 }
 
-function countDown(i) {
-    while (i >= 0) {
-        console.log(i)
-        i--
+function countDown (number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
     }
 }
